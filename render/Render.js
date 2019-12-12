1be13a675e2dafe072ff578666d03e8122542fb5
Maye Edwin <mayedwine1@gmail.com>
2019-12-12 16:19:32 +0300

    Add the Render()

--- render/Render.js
+++ render/Render.js
@@ -1,1 +1,22 @@
 // rendering components
+const Render = (doc) => {
+   // add the render components
+   // to get a particular value of a document, use "doc.data(value)"  
+}
+
+// get firestore documents 
+db.collection("collection").where("condition", "==", true)
+    .get()
+    .then((querySnapshot) => {
+
+        // loop through each document
+        querySnapshot.forEach((doc) => {
+
+           // call the "Render" function and pass "doc" as the parameter
+           Render(doc)
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
