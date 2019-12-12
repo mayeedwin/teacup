46197842758a74776cd78e66d69a124a1dafe39a
Glitch (teacupjs) <none>
2019-12-12 13:48:45 +0000

    🍉🐮 latest RC

--- render/Render.js
+++ render/Render.js
@@ -1,23 +1,20 @@
 // rendering components
-const Render = (doc) => {
-   // add the render components
-   // to get a particular value of a document, use "doc.data().value"  
-}
+const Render = doc => {
+  // add the render components
+  // to get a particular value of a document, use "doc.data().value"
+};
 
-// get firestore documents 
+// get firestore documents
 db.collection("collection")
-    .get()
-    .then((querySnapshot) => {
-
-        // loop through each document
-        querySnapshot.forEach((doc) => {
-
-           // call the "Render" function and pass "doc" as the parameter
-           Render(doc)
-        });
-    })
-    .catch((error) => {
-        // do something with the error
-        console.log("Error getting documents: ", error);
+  .get()
+  .then(querySnapshot => {
+    // loop through each document
+    querySnapshot.forEach(doc => {
+      // call the "Render" function and pass "doc" as the parameter
+      Render(doc);
     });
-
+  })
+  .catch(error => {
+    // do something with the error
+    console.log("Error getting documents: ", error);
+  });
